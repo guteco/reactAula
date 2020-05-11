20ea6d210c1fe6e7382e1f0db58e9203baa3dac5
guteco <guteco@gmail.com>
2020-05-11 08:46:55 -0300

    Frontend terminado

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // com o código <Route path="/" component={Logon} />
 import Logon from './pages/Logon';
 import Register from './pages/Register';
+import Profile from './pages/Profile';
+import NewIncident from './pages/NewIncident';
 
 export default function Routes() {
     return (
@@ -13,6 +15,8 @@ export default function Routes() {
             <Switch>
                 <Route path="/" exact component={Logon} />
                 <Route path="/register" component={Register} />
+                <Route path="/profile" component={Profile} />
+                <Route path="/incidents/new" component={NewIncident} />
             </Switch>
         </BrowserRouter>
     );
